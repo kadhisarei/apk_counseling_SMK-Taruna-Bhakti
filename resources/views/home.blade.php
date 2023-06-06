7611e844842704c02bfdb74a0b64a8d51b838e1c
ibrahimkhalish <ibrahimkhalish@gmail.com>
2023-06-07 00:33:15 +0700

    tes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,7 +47,10 @@
             <p>Testimoni</p>
         </div>
     </div>
-
+    
+    
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+    <lottie-player src="{{ asset('lottie/main_comp.json') }}"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop autoplay></lottie-player>
     @include('components.footer')
     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
     <script src="{{ asset('js/script.js') }}"></script>
