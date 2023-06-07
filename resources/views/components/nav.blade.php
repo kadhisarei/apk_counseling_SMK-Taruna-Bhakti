7a4782ec7f81b8629a3e2029f248d581415fa803
@AkbarDz <rabbaniakbar4@gmail.com>
2023-06-07 09:50:53 +0700

    revise

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -6,7 +6,23 @@
         <ul>
             <li><a href="{{ url('/landing') }}">Beranda</a></li>
             <li><a href="#">Tentang</a></li>
-            <li><a href="{{ url('/student') }}">Konseling</a></li>
+            {{-- <li><a href="{{ url('/student') }}">Konseling</a></li> --}}
+            <li><a href="#">Login</a></li>
+            <li>
+            <button class="notif-button" onclick="notifOpen()">
+                <i class="fa fa-bell"></i>
+            </button>
+            <div class="notif-popup" id="notif">
+                <div>
+                    <h3>Pesan Baru</h3>
+                    <p class="message">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo minima dicta aspernatur, repellat minus ipsum, molestias, deleniti ratione ipsam non expedita. Autem labore asperiores expedita sequi, cupiditate omnis ipsam nam!</p>
+                </div>
+                
+                <button onclick="notifClose()">
+                    TUTUP
+                </button>
+            </div>
+            </li>
         </ul>
     </div>
 </nav>
